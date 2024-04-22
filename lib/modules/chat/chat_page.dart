b17b00a884fab1c437655db37f9d5ff6d8244d09
name <doe@example.com>
2024-04-23 04:13:12 +0500

    idkhelp

--- lib/modules/chat/chat_page.dart
+++ lib/modules/chat/chat_page.dart
@@ -10,10 +10,17 @@ class ChatPage extends GetView<ChatController> {
         appBar: AppBar(title: Text('Чат')),
         body: Column(
           children: [
-            Expanded(child: ListView.builder(itemBuilder: (context, index) {
-
-            })),
-            Container(height: 50 ,color: Color.fromARGB(255, 204, 239, 249),)
+            Expanded(
+                child: ListView.builder(
+                    itemCount: controller.messages.length,
+                    itemBuilder: (context, index) {
+                      var message = controller.messages[index];
+                      return Text("${message.username} ${message.message}");
+                    })),
+            Container(
+              height: 50,
+              color: Color.fromARGB(255, 204, 239, 249),
+            )
           ],
         ));
   }
--- lib/services/socket_service.dart
+++ lib/services/socket_service.dart
@@ -21,20 +21,25 @@ class SocketService extends GetxService {
 
     _socket.onConnect((data) {
       printInfo(info: "Socket connected");
+      _sendLogInMesssage();
       Get.offNamed(Routes.CHAT);
     });
-   
-    _socket.onDisconnect((data) => printInfo(info: "Disconected"));
-   
-    _socket.onConnectError((data) => printInfo(info: "Connection error"));
-   
-    _socket.onAny((event, data) {
-      var isKnown = SocketEvent.values.any((el) => el.name == event);
-      if (!isKnown) return;
-      data['type'] = event;
-      var message = ChatMessage.fromJson(data);
-      UserService.to.addMessageToList(message);
+
+    _socket.onDisconnect((data) {
+      printInfo(info: "Disconnected");
+      UserService.to.clearMessage();
+      Get.offNamed(Routes.HOME);
     });
+
+    _socket.onConnectError((data) => printInfo(info: "Connection error"));
+
+    _socket.onAny((event, data) { 
+      var isKnown = SocketEvent.values.any((el) => el.name == event); 
+      if (!isKnown) return; 
+      data['type'] = event; 
+      var message = ChatMessage.fromJson(data); 
+      UserService.to.addMessageToList(message); 
+    }); 
 
     return this;
   }
@@ -46,4 +51,13 @@ class SocketService extends GetxService {
   void disconnect() {
     _socket.disconnect();
   }
+
+  void _sendLogInMesssage() {
+    _socket.emit(SocketEvent.login.name, UserService.to.username);
+  }
+
+  void _sendLogOutMesssage() {
+    _socket.emit(SocketEvent.logout.name);
+  }
+
 }
