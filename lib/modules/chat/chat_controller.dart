e87502f52c063b825a8e825d6189e871e41f418f
name <doe@example.com>
2024-04-23 03:59:34 +0500

    chat1

--- lib/modules/chat/chat_controller.dart
+++ lib/modules/chat/chat_controller.dart
@@ -1,5 +1,37 @@
+import 'package:chat/models/chatMessage/chat_message.dart';
+import 'package:chat/services/user_service.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 class ChatController extends GetxController {
+  
+  RxList<ChatMessage> get messages => UserService.to.messages;
+
+  final textCtrl = TextEditingController();
+  final scrollCtrl = ScrollController();
+  final focusNode = FocusNode();
+
+  @override
+  void onInit() {
+    messages.listen(
+      (p0) async {
+        if (scrollCtrl.hasClients) {
+          var max = scrollCtrl.position.maxScrollExtent;
+          if (scrollCtrl.offset + 100 >= max) {
+            await Future.delayed(
+              const Duration(milliseconds: 100),
+              () {
+                scrollCtrl.animateTo(scrollCtrl.position.maxScrollExtent,
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeOut);
+              },
+            );
+          }
+        }
+      },
+    );
+    super.onInit();
+  }
+
 
 }
--- lib/modules/chat/chat_page.dart
+++ lib/modules/chat/chat_page.dart
@@ -4,15 +4,17 @@ import 'package:get/get.dart';
 import 'chat_controller.dart';
 
 class ChatPage extends GetView<ChatController> {
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-    appBar: AppBar(title: Text('ChatPage')),
+        appBar: AppBar(title: Text('Чат')),
+        body: Column(
+          children: [
+            Expanded(child: ListView.builder(itemBuilder: (context, index) {
 
-    body: SafeArea(
-      child: Text('ChatController'))
-    );
+            })),
+            Container(height: 50 ,color: Color.fromARGB(255, 204, 239, 249),)
+          ],
+        ));
   }
 }
